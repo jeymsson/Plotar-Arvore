1313da8f36f76225ec18febdee35e39e3d51d8a1
Jeymsson <jeymsson@hotmail.com>
2016-05-14 11:43:32 -0300

    Adequação para utilização em arvores Adições:   Variáveis.     - Pai     - Idade     - Nome (1-3)     - nume_strings     - Sentinel   Funções.     - getNumStrings     - verificaNumStrings (faz a contagem)     - get e set Left     - get e set Right     - get e set Pai     - verificação se é Raiz     - verificação se é Sentinela     - getIdade     - getNomes     - getNome (mandando a posição 0+1)     - Set nome (mandando a posição)

--- nol.cpp
+++ nol.cpp
@@ -1,6 +1,20 @@
 #include "nol.h"
 
 
+Nol::Nol()
+{
+	this->Next = NULL;
+	this->Back = NULL;
+	this->Pai = NULL;
+	this->Sentinel = true;
+	int num_string = 0;
+}
+
+Nol::~Nol()
+{
+}
+
+
 void Nol::ordena()
 {
     if ( !(this->nome3.empty()) ) {
@@ -24,30 +38,22 @@ void Nol::ordena()
     }
 }
 
-Nol::Nol()
-{
-    this->Next = NULL;
-    this->Back = NULL;
-}
-
-Nol::~Nol()
-{
-}
-
-
 void Nol::setTexto(string & texto)
 {
-    ordena();
-    if (this->nome1.empty()) {
-        this->nome1 = texto;
-    } else if (this->nome2.empty()) {
-        this->nome2 = texto;
-    } else if (this->nome3.empty()) {
-        this->nome3 = texto;
-    }
-    else {
-        cout << "\nNúmero máximo de nomes atingido no Nol";
-    }
+	verificaNumStrings();
+	ordena();
+	if (this->nome1.empty()) {
+		this->nome1 = texto;
+	}
+	else if (this->nome2.empty()) {
+		this->nome2 = texto;
+	}
+	else if (this->nome3.empty()) {
+		this->nome3 = texto;
+	}
+	else {
+		cout << "\nNúmero máximo de nomes atingido no Nol";
+	}
 }
 
 void Nol::setCoord_x(int x)
@@ -70,6 +76,29 @@ int Nol::getCoord_y()
     return this->coord_y;
 }
 
+int Nol::getNumStrings()
+{
+	verificaNumStrings();
+	return this->num_strings;
+}
+
+void Nol::verificaNumStrings()
+{
+	ordena();
+	if (!this->nome3.empty()) {
+		this->num_strings = 3;
+	}
+	else if (!this->nome2.empty()) {
+		this->num_strings = 2;
+	}
+	else if (!this->nome1.empty()) {
+		this->num_strings = 1;
+	}
+	else {
+		this->num_strings = 0;
+	}
+}
+
 Nol * Nol::getNext()
 {
     return this->Next;
@@ -80,6 +109,47 @@ Nol * Nol::getBack()
     return this->Back;
 }
 
+Nol * Nol::getLeft()
+{
+    return getBack();
+}
+
+Nol * Nol::getRight()
+{
+    return getNext();
+}
+
+Nol * Nol::getPai()
+{
+    return this->Pai;
+}
+
+void Nol::setLeft(Nol * left)
+{
+    this->Back = left;
+}
+
+void Nol::setRight(Nol * right)
+{
+    this->Next = right;
+}
+
+void Nol::setPai(Nol * pai)
+{
+    this->Pai = pai;
+}
+
+bool Nol::isRoot()
+{
+    return (getPai() == NULL);
+}
+
+bool Nol::isSentinel()
+{
+    ((getLeft() == NULL  &&  getRight() == NULL) ? this->Sentinel = true : this->Sentinel = false);
+    return (getLeft() == NULL  &&  getRight() == NULL);
+}
+
 int Nol::getIdade()
 {
     return this->Idade;
@@ -88,11 +158,13 @@ int Nol::getIdade()
 string Nol::getNomes()
 {
     this->ordena();
-    if(!(this->nome3.empty()) ){
+    if (!(this->nome3.empty())) {
         return string(this->nome1 + ", " + this->nome2 + ", " + this->nome3);
-    } else if(!(this->nome2.empty()) ){
+    }
+    else if (!(this->nome2.empty())) {
         return string(this->nome1 + ", " + this->nome2);
-    } else if(!(this->nome1.empty()) ){
+    }
+    else if (!(this->nome1.empty())) {
         return string(this->nome1);
     }
     return string("");
@@ -104,15 +176,18 @@ string Nol::getNome(int NumeroNome)
     this->ordena();
 
     //NumeroNome nao pode ser < 3, já que tem so 3 nomes
-    if(NumeroNome > 0  || NumeroNome < 3){
-               if(NumeroNome == 1 ){
-            print=  this->nome1;
-        } else if(NumeroNome == 2 ){
-            print=  this->nome2;
-        } else if(NumeroNome == 3 ){
-            print=  this->nome3;
+    if (NumeroNome > 0 || NumeroNome < 3) {
+        if (NumeroNome == 1) {
+            print = this->nome1;
         }
-    } else {
+        else if (NumeroNome == 2) {
+            print = this->nome2;
+        }
+        else if (NumeroNome == 3) {
+            print = this->nome3;
+        }
+    }
+    else {
         print = "";
     }
     return print;
@@ -121,15 +196,18 @@ string Nol::getNome(int NumeroNome)
 void Nol::setNome(int NumeroNome, string texto)
 {
     //NumeroNome nao pode ser < 3, já que tem so 3 nomes
-    if(NumeroNome > 0  || NumeroNome < 3){
-               if(NumeroNome == 1 ){
+    if (NumeroNome > 0 || NumeroNome < 3) {
+        if (NumeroNome == 1) {
             this->nome1 = texto;
-        } else if(NumeroNome == 2 ){
+        }
+        else if (NumeroNome == 2) {
             this->nome2 = texto;
-        } else if(NumeroNome == 3 ){
+        }
+        else if (NumeroNome == 3) {
             this->nome3 = texto;
         }
-    } else {
+    }
+    else {
         cout << "Não adicionado numero fora dos limites";
     }
 }
--- nol.h
+++ nol.h
@@ -6,39 +6,56 @@
 
 using namespace std;
 
-class Nol{
+class Nol {
 
 protected: //Coordenadas de cada no
-    int coord_x;
-    int coord_y;
+	int coord_x;
+	int coord_y;
 private:
-    Nol * Next;
-    Nol * Back;
-    int Idade;
-    string nome1;
-    string nome2;
-    string nome3;
+	Nol * Next;
+	Nol * Back;
+	Nol * Pai;
+	int Idade;
+	string nome1;
+	string nome2;
+	string nome3;
+	int num_strings;
+	bool Sentinel;
 
 public:
-    Nol();
-    ~Nol();
-    void ordena();
-    void setTexto(string & texto);
+	Nol();
+	~Nol();
+	void ordena();
+	void setTexto(string & texto);
 
-    void setCoord_x(int x);
-    int getCoord_x();
-    void setCoord_y(int y);
-    int getCoord_y();
+	void setCoord_x(int x);
+	int getCoord_x();
+	void setCoord_y(int y);
+	int getCoord_y();
+	int getNumStrings();
+	void verificaNumStrings();
 
-    Nol * getNext();
-    Nol * getBack();
+	//Para Pilha
+	Nol * getNext();
+	Nol * getBack();
 
-    int getIdade();
-    string getNomes();
-    string getNome(int NumeroNome);
-    void   setNome(int NumeroNome, string texto);
+	//Para Arvore
+	Nol * getLeft();
+	Nol * getRight();
+	Nol * getPai();
+	void setLeft(Nol * left);
+	void setRight(Nol * right);
+	void setPai(Nol * pai);
+	bool isRoot();
+	bool isSentinel();
 
-    friend class Pilha;
+	int getIdade();
+	string getNomes();
+	string getNome(int NumeroNome);
+	void   setNome(int NumeroNome, string texto);
+
+	friend class Pilha;
+	friend class Arvore;
 };
 
 #endif // NOL_H
