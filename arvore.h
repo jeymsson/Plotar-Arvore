3380d34929e68446bfeeb0ee8047c741eb7c00f7
Jeymsson <jeymsson@hotmail.com>
2016-05-30 01:37:45 -0300

    Functions Pop, clear, destructor added, corrections in VLR, LVR, LRV.

--- arvore.h
+++ arvore.h
@@ -8,47 +8,50 @@
 class Arvore
 {
 private:
-	Nol * mae;
-	Nol * raiz;
-	int qtd_Nos;
-	
-	Pilha * varredura;
-	void VLR(Nol * No, Pilha * p);
-	void LVR(Nol * No, Pilha * p);
-	void LRV(Nol * No, Pilha * p);
+    Nol * mae;
+    Nol * raiz;
+    int qtd_Nos;
 
-	//Altura e largura
-	int  retornarAltura(Nol * r);
-	int  getWidth(Nol* root, int level);
-	
+    Pilha * varredura;
+    void VLR(Nol * No, Pilha * p);
+    void LVR(Nol * No, Pilha * p);
+    void LRV(Nol * No, Pilha * p);
+
+    //Altura e largura
+
+    int  getWidth(Nol* root, int level);
+    int  getNum_Folhas(Nol* r, int & n);
+
 public:
-	Arvore();
-	~Arvore();
-	
-	void  expandSentinel(Nol * sentinela);
-	
-		//Gets
-	Nol * getRaiz();
-	Pilha* getVarredura();
-	int    getQtd_Nos();
-		//Busca
-	Nol* Busca_no(int chave);
-		//Adição
-	void Add_org(int idade, string & nome);
-	void Add_No(Nol* No);
-	
-		//Vareduras
-	void VLR();
-	void LVR();
-	void LRV();
+    Arvore();
+    ~Arvore();
+
+    void  expandSentinel(Nol * sentinela);
+
+        //Gets
+    Nol * getRaiz();
+    Pilha* getVarredura();
+    int    getQtd_Nos();
+        //Busca
+    Nol* Busca_no(int chave);
+        //AdiÃ§Ã£o
+    void Add_org(int idade, string & nome);
+    void Add_No(Nol* No);
+
+        //Vareduras
+    void VLR();
+    void LVR();
+    void LRV();
 
 
-	int  getAltura();
-	int  getLargura();
-	int  getMaxLargura(Nol* root);
+    int  getAltura();
+    int  retornarAltura(Nol * r);
+    int  getLargura();
+    int  getMaxLargura(Nol* root);
+    int  getNum_Folhas();
 
-	
-	
+    void Pop(int chave);
+    void clear();
 
 };
 
